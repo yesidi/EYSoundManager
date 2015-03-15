4ba2d7862cd94c73696560d93600367f19337542
Yip <yesidi@me.com>
2015-03-15 15:31:23 +0800

    Update

--- EYSoundManager/EYSoundManager.h
+++ EYSoundManager/EYSoundManager.h
@@ -9,23 +9,35 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
-typedef NS_ENUM (int, EYSOUNDMANAGER_STATUS) {
-    EYSOUNDMANAGER_STATUS_PLAYING,
-    EYSOUNDMANAGER_STATUS_PAUSE,
-    EYSOUNDMANAGER_STATUS_FINISHED,
-    EYSOUNDMANAGER_STATUS_STOPED,
-    EYSOUNDMANAGER_STATUS_LOADING,
-    EYSOUNDMANAGER_STATUS_FAILED,
+typedef NS_ENUM (int, EYSOUND_AUDIO_STATUS) {
+    EYSOUND_AUDIO_STATUS_PLAYING,
+    EYSOUND_AUDIO_STATUS_PAUSE,
+    EYSOUND_AUDIO_STATUS_FINISHED,
+    EYSOUND_AUDIO_STATUS_STOPED,
+    EYSOUND_AUDIO_STATUS_LOADING,
+    EYSOUND_AUDIO_STATUS_FAILED,
 };
-typedef void (^periodicBlock)(Float64 degree, Float64 elapsedTime, Float64 timeRemaining, NSError *error, EYSOUNDMANAGER_STATUS status);
 
-@interface EYSoundManager : NSObject
+typedef NS_ENUM (int, EYSOUND_RECORD_STATUS) {
+    EYSOUND_RECORD_STATUS_RECORDING,
+    EYSOUND_RECORD_STATUS_PAUSE,
+    EYSOUND_RECORD_STATUS_STOP,
+    EYSOUND_RECORD_STATUS_FINISHED,
+};
+typedef void (^periodicBlock)(Float64 degree, Float64 elapsedTime, Float64 timeRemaining, NSError *error, EYSOUND_AUDIO_STATUS status);
+typedef void (^recordBlock)(NSTimeInterval currentTime, Float64 averagePower, EYSOUND_RECORD_STATUS status);
+
+@interface EYSoundManager : NSObject<AVAudioRecorderDelegate>
 @property (nonatomic, strong) AVAudioPlayer *audioPlayer;
 @property (nonatomic, strong) AVPlayer *player;
+@property (nonatomic, strong) AVAudioRecorder *recorder;
 @property (nonatomic, copy) periodicBlock block;
-@property (nonatomic) EYSOUNDMANAGER_STATUS status;
+@property (nonatomic, copy) recordBlock eyRecordBlock;
+@property (nonatomic) EYSOUND_AUDIO_STATUS status;
+@property (nonatomic) EYSOUND_RECORD_STATUS recordStatus;
 @property (nonatomic, strong) NSString *currentStreamingURL;
 @property (nonatomic, strong) NSString *tag;
+@property (nonatomic, strong) CADisplayLink *displaylink;
 
 +(EYSoundManager *)sharedManager;
 -(void)startStreamingAudioWithURL:(NSString *)url tag:(NSString *)tag usingBlock:(periodicBlock)block;
@@ -33,4 +45,12 @@ typedef void (^periodicBlock)(Float64 degree, Float64 elapsedTime, Float64 timeR
 -(void)stop;
 -(void)replay;
 -(void)play;
+
+//record
+-(void)startRecordingWithFileName:(NSString *)name andExtension:(NSString *)extension forDuration:(NSTimeInterval)second usingBlock:(recordBlock)block;
+-(void)pauseRecording;
+-(void)resumeRecording;
+-(void)stopAndSaveRecording;
+-(void)cancelRecording;
+
 @end
--- EYSoundManager/EYSoundManager.m
+++ EYSoundManager/EYSoundManager.m
@@ -35,10 +35,10 @@
     [_player play];
     if (playerItem.status == AVPlayerItemStatusUnknown) {
         NSLog(@"AVPlayerItemStatusUnknown");
-        _block(0.f, 0.f, 0.f, playerItem.error, EYSOUNDMANAGER_STATUS_LOADING);
+        _block(0.f, 0.f, 0.f, playerItem.error, EYSOUND_AUDIO_STATUS_LOADING);
     }else if(playerItem.status == AVPlayerItemStatusFailed) {
         NSLog(@"AVPlayerItemStatusFailed");
-        _block(0.f, 0.f, 0.f, playerItem.error, EYSOUNDMANAGER_STATUS_FAILED);
+        _block(0.f, 0.f, 0.f, playerItem.error, EYSOUND_AUDIO_STATUS_FAILED);
         _currentStreamingURL = nil;
         _tag = nil;
     }
@@ -77,13 +77,13 @@
     if ([keyPath isEqualToString:@"status"]) {
         AVPlayerItem *playerItem = (AVPlayerItem*)object;
         if (playerItem.status==AVPlayerStatusReadyToPlay) {
-            _status = EYSOUNDMANAGER_STATUS_PLAYING;
+            _status = EYSOUND_AUDIO_STATUS_PLAYING;
         }else if (playerItem.status==AVPlayerStatusFailed) {
-            _status = EYSOUNDMANAGER_STATUS_FAILED;
+            _status = EYSOUND_AUDIO_STATUS_FAILED;
             _currentStreamingURL = nil;
             _tag = nil;
         }else if (playerItem.status==AVPlayerStatusUnknown) {
-            _status = EYSOUNDMANAGER_STATUS_LOADING;
+            _status = EYSOUND_AUDIO_STATUS_LOADING;
             _currentStreamingURL = nil;
             _tag = nil;
         }
@@ -92,7 +92,7 @@
 
 -(void)didPlayToEndTime:(NSNotification *)sender
 {
-    _status = EYSOUNDMANAGER_STATUS_FINISHED;
+    _status = EYSOUND_AUDIO_STATUS_FINISHED;
     _currentStreamingURL = nil;
     _tag = nil;
     Float64 durationTime = CMTimeGetSeconds([(AVPlayerItem *)[sender object] duration]);
@@ -104,7 +104,7 @@
 {
     NSLog(@"failedToPlayToEndTime--%@", _player.currentItem.error.description);
     [[NSNotificationCenter defaultCenter]removeObserver:self name:AVPlayerItemFailedToPlayToEndTimeNotification object:sender.object];
-    _status = EYSOUNDMANAGER_STATUS_FAILED;
+    _status = EYSOUND_AUDIO_STATUS_FAILED;
     _currentStreamingURL = nil;
     _tag = nil;
 }
@@ -115,16 +115,16 @@
     NSLog(@"playbackStalled--%@", _player.currentItem.error.description);
 }
 
-//method
+//audio play method
 -(void)pause
 {
     [_player pause];
-    _status = EYSOUNDMANAGER_STATUS_PAUSE;
+    _status = EYSOUND_AUDIO_STATUS_PAUSE;
 }
 
 -(void)stop
 {
-    _status = EYSOUNDMANAGER_STATUS_STOPED;
+    _status = EYSOUND_AUDIO_STATUS_STOPED;
     _currentStreamingURL = nil;
     _tag = nil;
     [_player pause];
@@ -133,14 +133,104 @@
 
 -(void)play
 {
-    _status = EYSOUNDMANAGER_STATUS_PLAYING;
+    _status = EYSOUND_AUDIO_STATUS_PLAYING;
     [_player play];
 }
 
 -(void)replay
 {
     [_player seekToTime:CMTimeMake(0, 1)];
-    _status = EYSOUNDMANAGER_STATUS_PLAYING;
+    _status = EYSOUND_AUDIO_STATUS_PLAYING;
     [_player play];
 }
+
+//record method
+-(void)startRecordingWithFileName:(NSString *)name andExtension:(NSString *)extension forDuration:(NSTimeInterval)second usingBlock:(recordBlock)block
+{
+    if (_recorder.isRecording)
+        [self cancelRecording];
+    
+    _eyRecordBlock = [block copy];
+    self.displaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateMeters)];
+
+    NSDictionary *settings=[NSDictionary dictionaryWithObjectsAndKeys:
+                            [NSNumber numberWithInt:kAudioFormatMPEG4AAC],
+                            AVFormatIDKey,
+                            [NSNumber numberWithFloat:44100.f],
+                            AVSampleRateKey,
+                            [NSNumber numberWithInt:1],
+                            AVNumberOfChannelsKey, nil];
+    
+    NSError *recorderError = nil;
+    NSError *sessionError = nil;
+
+    _recorder = [[AVAudioRecorder alloc]initWithURL:[NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/%@.%@", NSTemporaryDirectory(), name, extension]] settings:settings error:&recorderError];
+    _recorder.delegate = self;
+
+    if (recorderError) {
+        NSLog(@"recorderError:%@", recorderError.description);
+        return;
+    }
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:&sessionError];
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
+
+    if (sessionError) {
+        NSLog(@"sessionError:%@", sessionError.description);
+        return;
+    }
+    [_recorder setMeteringEnabled:YES];
+    [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    if (second == 0 && !second) {
+        [_recorder record];
+    } else {
+        [_recorder recordForDuration:second];
+    }
+}
+
+- (void)updateMeters
+{
+    [_recorder updateMeters];
+    _eyRecordBlock(_recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_RECORDING);
+}
+
+-(void)pauseRecording {
+    if ([_recorder isRecording]) {
+        [_recorder pause];
+        [self.displaylink setPaused:YES];
+        _eyRecordBlock(_recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_PAUSE);
+    }
+}
+
+-(void)resumeRecording {
+    if (![_recorder isRecording]) {
+        [_recorder record];
+        [self.displaylink setPaused:NO];
+        _eyRecordBlock(_recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_RECORDING);
+    }
+}
+
+-(void)stopAndSaveRecording {
+    _recorder.delegate = nil;
+    [self.displaylink setPaused:YES];
+    [_recorder stop];
+    _eyRecordBlock(_recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_STOP);
+}
+
+-(void)cancelRecording {
+    _recorder.delegate = nil;
+    [self.displaylink setPaused:YES];
+    [_recorder stop];
+    [_recorder deleteRecording];
+    _eyRecordBlock(_recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_STOP);
+}
+
+-(void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag
+{
+    if (flag)
+    {
+        [self.displaylink setPaused:YES];
+        _eyRecordBlock(recorder.currentTime, [_recorder averagePowerForChannel:0], EYSOUND_RECORD_STATUS_FINISHED);
+    }
+}
+
 @end
